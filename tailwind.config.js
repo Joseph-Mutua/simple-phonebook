265f901436f44c8fd87283daa7eaad7c8eae5765
Joseph Mutua <Mutuaj793@gmail.com>
2023-12-24 11:06:04 +0300

    Minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors: {
@@ -56,5 +56,5 @@ module.exports = {
       ],
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
